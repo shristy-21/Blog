f13fdb3dffb0a0de7492f32da57789611176dfbd
shristy-21 <shristyk.969@gmail.com>
2024-01-24 15:03:30 +0530

    add nav

--- Blog/settings.py
+++ Blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9u&33p&hw#2j+0q!j0vjdb((9xozoe-l&de@yqt1wr+pyh!ao=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','vercel.app']
 
 
 # Application definition
--- Blog/urls.py
+++ Blog/urls.py
@@ -4,5 +4,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('article',include('article.urls'))
+    path('',include('article.urls'))
 ]
