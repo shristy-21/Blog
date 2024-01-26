1e4eac28cc60348119c621a30d62d25e5a01fdc7
shristy-21 <shristyk.969@gmail.com>
2024-01-26 12:09:09 +0530

    add

--- article/models.py
+++ article/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class  Article(models.Model):
+    Title=models.CharField(max_length=100)
+    Content=models.TextField(max_length=200)
+    img=models.ImageField(upload_to='article_images/',default='default_img.jpg')
+    pub_date=models.DateField(auto_now=True)
+
+
+    def __str__(self):
+        return self.Title
